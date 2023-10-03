86296fa8f27a46b2bbc644e99d495641ba87e6a4
daianasilvera <daiana.silvera@estudiantes.utec.edu.uy>
2023-10-02 22:17:58 -0300

    Esqueleto diseño ActivityMain, Login, Registro Fragments Home, Perfil, Constancias

--- Mobile/app/src/main/java/com/utec/proyectofinaltecnicatura/MainActivity.kt
+++ Mobile/app/src/main/java/com/utec/proyectofinaltecnicatura/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.utec.proyectofinaltecnicatura
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var nav: NavigationBarView
+    private  lateinit var testLinkRegistro: TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var vista= Intent(this, Login::class.java)
+        startActivity(vista)
+
+
+
+
+
+        nav = findViewById<BottomNavigationView>(R.id.nav)
+        nav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home-> supportFragmentManager.beginTransaction().replace(R.id.contenedor, Home()).commit()
+                R.id.perfil -> supportFragmentManager.beginTransaction().replace(R.id.contenedor, Perfil()).commit()
+                R.id.constancias -> supportFragmentManager.beginTransaction().replace(R.id.contenedor, Constancias()).commit()
+            }
+            true
+        }
+
+        if(savedInstanceState == null)
+            supportFragmentManager.beginTransaction().replace(R.id.contenedor, Home()).commit()
     }
+
+
 }
