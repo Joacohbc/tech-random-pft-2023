da565ee0b6b3a92615c667bfc9ee07bd7f9e6d12
william-lti <106108186+william-lti@users.noreply.github.com>
2023-11-03 11:36:39 -0300

    Se agrega ingresar reclamo para estudiatne
    Tiene error en el detalle ya que no lo está tomando

--- Web/JavaSource/test/Main.java
+++ Web/JavaSource/test/Main.java
@@ -16,26 +16,27 @@ public class Main {
 		
 		JWTUtils jwtUtils = new JWTUtils();
 		
+<<<<<<< Updated upstream
 		UserDetails details = new UserDetails();
 		details.setIdRol(2l);
 		details.setIdUsuario(5l);
 		details.setNombreUsuario("joaquin.genova");
 		details.setRol(Rol.TUTOR);
 		System.out.print(jwtUtils.generateToken(details));
+=======
+		//System.out.println(jwtUtils.isTokenExpired("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiMWVlYzJkZS0zNjMyLTRkZWYtODQzNi01MDQ2OWFjNjFjODQiLCJleHAiOjE2OTcyMDU5MTIsImlhdCI6MTY5NzIwNTYxMn0.vP8cR7flqbYgXkPfT5plNHKsKh4PgyRjodihgoM2Xxe6Ww4p3LaTvVQObTCvvEnvKyNP8qj9BaFJIYNpV71r7g"));
+>>>>>>> Stashed changes
 		
 //		System.out.println(jwtUtils.getUserDetails("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJKb2FxdWluIiwiaWRSb2wiOjEsImlkVXN1YXJpbyI6MSwiZXhwIjoxNjg4NTE1MTUxLCJpYXQiOjE2ODg1MTE1NTEsInJvbCI6IlRVVE9SIn0.e5rjNlMRVHmK-jldVJzdR3xHinhtrD2mA2WI44-OjlewBvT3VIvQAmXX4Q3_Xf-ruFpifD4kpjsFLzsi1Fsbqw"));
 		
-//		final String userPassword = "Mieqweqwmega epicae123123wdeqwe qwewqfgeqqweqw321312ljeraglsareml brem lgml ;ea;ktkqerwerewrdsgnaerll";
-//
-////		
-//		final Argon2Function hashFunction = Argon2Function.getInstance(1024, 3, 2, 64, Argon2.ID);
-////		
-//		String hash = Password.hash("1234567Ab")
-//				.addRandomSalt(64)
-//				.addPepper("1234567Ab")
-//				.with(hashFunction).getResult();
-//		
-//		System.out.println(hash);
+		final String userPassword = "Mieqweqwmega epicae123123wdeqwe qwewqfgeqqweqw321312ljeraglsareml brem lgml ;ea;ktkqerwerewrdsgnaerll";		
+		final Argon2Function hashFunction = Argon2Function.getInstance(1024, 3, 2, 64, Argon2.ID);		
+		String hash = Password.hash("1234567Ab")
+				.addRandomSalt(64)
+				.addPepper("1234567Ab")
+				.with(hashFunction).getResult();
+		
+		System.out.println(hash);
 //		
 //		System.out.println(Password.check("1234567Ab", hash)
 //				.addPepper("1234567Ab")
